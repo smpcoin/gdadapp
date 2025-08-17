26381a0a7c81d5144260df17d27ea31e1170bb00
wx_鲎ec9d <8122470+wx_e66042ab80@user.noreply.gitee.com>
2025-08-17 13:59:30 +0800

    '

---
+++ assets/cursor-D2h5uDng.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-81Szfllj.js";import"./index-CxQ8DCPP.js";import"./index.es-Ce9-LymI.js";import"./events-DQ172AOg.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
