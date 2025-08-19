32a889e7e8cc81e1494bf90e5f1a4759e58cbe90
wx_鲎ec9d <8122470+wx_e66042ab80@user.noreply.gitee.com>
2025-08-19 09:20:50 +0800

    '

---
+++ assets/chevron-right-9K3-9sGc.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-C3XrCfV5.js";import"./index-dt6me3HK.js";import"./index.es-Dd_rNCYp.js";import"./events-DQ172AOg.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{t as chevronRightSvg};
